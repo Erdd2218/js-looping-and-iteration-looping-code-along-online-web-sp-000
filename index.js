416217ae20ea475f07941cbc74fb43e9b9af266c
Andre Pierre <andre.laurence94@gmail.com>
2021-05-16 21:36:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, cards) {
+  let thankYou = []
+  for (let i = 0; i < names.length; i++) {
+    thankYou.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+  }
+  return thankYou
+}
+
+function countDown(num) {
+  for (let i = 0; num > 0; i--) {
+    
+  }
+  console.log(num)
+}
